537889fb25caf3eafcc9f98c134320dae053de9a
Sam Faull <samz5991@hotmail.co.uk>
2020-05-05 11:25:36 +0100

    Removed Wifi credentials from config flie - these are no longer used in the wifi setup process in favour of wifimanager

--- SensorNode/CredentialsConfig-template.h
+++ SensorNode/CredentialsConfig-template.h
@@ -1,8 +1,3 @@
-// Wifi
-#define WIFI_SSID       "your ssid here"
-#define WIFI_PASSWORD   "your pass here"
-
-
 //MQTT
 #define MQTT_SERVER     "your mqtt server here"
 #define MQTT_USERNAME   "your username here"
