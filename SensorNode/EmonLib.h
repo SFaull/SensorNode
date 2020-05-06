d2b569d8edffa41034726ba3205a92f8299b2a83
Sam Faull <samz5991@hotmail.co.uk>
2020-05-06 11:34:18 +0100

    Copied EmonLib into project and modified to include a callback for the current sense ADC read process (this allows me to define a function to read an external ADC, rather than the internal one)

---
+++ SensorNode/EmonLib.h
@@ -0,0 +1,97 @@
+/*
+  Emon.h - Library for openenergymonitor
+  Created by Trystan Lea, April 27 2010
+  GNU GPL
+  modified to use up to 12 bits ADC resolution (ex. Arduino Due)
+  by boredman@boredomprojects.net 26.12.2013
+  Low Pass filter for offset removal replaces HP filter 1/1/2015 - RW
+*/
+
+#ifndef EmonLib_h
+#define EmonLib_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+
+#include "Arduino.h"
+
+#else
+
+#include "WProgram.h"
+
+#endif
+
+// define theoretical vref calibration constant for use in readvcc()
+// 1100mV*1024 ADC steps http://openenergymonitor.org/emon/node/1186
+// override in your code with value for your specific AVR chip
+// determined by procedure described under "Calibrating the internal reference voltage" at
+// http://openenergymonitor.org/emon/buildingblocks/calibration
+#ifndef READVCC_CALIBRATION_CONST
+#define READVCC_CALIBRATION_CONST 1126400L
+#endif
+
+
+#define ADC_COUNTS(x)  (1<<x)
+
+
+class EnergyMonitor
+{
+  public:
+    // added by Sam Faull
+    void setAdcResolution(unsigned int _resolution);
+    void setReadCallback(unsigned int(*callback)());
+    void voltage(unsigned int _inPinV, double _VCAL, double _PHASECAL);
+    void current(unsigned int _inPinI, double _ICAL);
+
+    void voltageTX(double _VCAL, double _PHASECAL);
+    void currentTX(unsigned int _channel, double _ICAL);
+
+    void calcVI(unsigned int crossings, unsigned int timeout);
+    double calcIrms(unsigned int NUMBER_OF_SAMPLES);
+    void serialprint();
+
+    long readVcc();
+    //Useful value variables
+    double realPower,
+      apparentPower,
+      powerFactor,
+      Vrms,
+      Irms;
+
+  private:
+    // added by Sam Faull
+    unsigned int (*readCallback)(void);
+    unsigned int adcResolution;
+    bool customCallback = false;
+    
+    //Set Voltage and current input pins
+    unsigned int inPinV;
+    unsigned int inPinI;
+    //Calibration coefficients
+    //These need to be set in order to obtain accurate results
+    double VCAL;
+    double ICAL;
+    double PHASECAL;
+
+    //--------------------------------------------------------------------------------------
+    // Variable declaration for emon_calc procedure
+    //--------------------------------------------------------------------------------------
+    int32_t sampleV;                        //sample_ holds the raw analog read value
+    int32_t sampleI;
+
+    double lastFilteredV,filteredV;          //Filtered_ is the raw analog value minus the DC offset
+    double filteredI;
+    double offsetV;                          //Low-pass filter output
+    double offsetI;                          //Low-pass filter output
+
+    double phaseShiftedV;                             //Holds the calibrated phase shifted voltage.
+
+    double sqV,sumV,sqI,sumI,instP,sumP;              //sq = squared, sum = Sum, inst = instantaneous
+
+    int startV;                                       //Instantaneous voltage at start of sample window.
+
+    boolean lastVCross, checkVCross;                  //Used to measure number of times threshold is crossed.
+
+
+};
+
+#endif
